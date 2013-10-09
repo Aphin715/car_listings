d4ec4a03e6a77409b05c69b1b9c0533abc74a12a
Alex Phinizy <AlexPhinizy@gmail.com>
2013-10-09 18:26:31 -0400

    Can add car and get errors

---
+++ spec/models/car_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Car do
+
+  #it { should belong_to(:owner)}
+
+  it { should have_valid(:color).when('red', 'blue') }
+  it { should_not have_valid(:color).when('', nil) }
+
+  it { should have_valid(:year).when(1980, 2013) }
+  it { should_not have_valid(:year).when('', nil, 1979, 2015) }
+
+  it { should have_valid(:mileage).when(1, 0, 100000, 40000) }
+  it { should_not have_valid(:mileage).when('', nil, '-1',) }
+
+
+
+end
